93ca7e9e0c42f159979a1094728a8915ae627b42
Aashiq-Otp <AashiqOtp.as@gmail.com>
2020-05-07 11:35:46 +0530

    Updates

---
+++ precache-manifest.b796922364d9d27ce345837320e78eb7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8b6f7cb3e222ffeb36a24ce542b539",
+    "url": "/covid19Kerala/index.html"
+  },
+  {
+    "revision": "7aafc5cc37217ca0094a",
+    "url": "/covid19Kerala/static/css/2.75dbc93a.chunk.css"
+  },
+  {
+    "revision": "7b2f1b66ab51ed805892",
+    "url": "/covid19Kerala/static/css/main.639d7a3d.chunk.css"
+  },
+  {
+    "revision": "7aafc5cc37217ca0094a",
+    "url": "/covid19Kerala/static/js/2.6b85fc68.chunk.js"
+  },
+  {
+    "revision": "6c722f06fd899c8438003bf0498b4172",
+    "url": "/covid19Kerala/static/js/2.6b85fc68.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b2f1b66ab51ed805892",
+    "url": "/covid19Kerala/static/js/main.5a2bbe79.chunk.js"
+  },
+  {
+    "revision": "1467a1dfd2c6b6195a0d",
+    "url": "/covid19Kerala/static/js/runtime-main.275377c3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid19Kerala/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid19Kerala/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid19Kerala/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid19Kerala/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid19Kerala/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
